1b2bfaeecf12fecac83ebd38996d847d37536e53
Bart-15 <brat.tabusao@gmail.com>
2025-08-17 16:32:54 +0800

    fix: improve S3 bucket permissions handling

--- cdk/lib/api-gw-stack.ts
+++ cdk/lib/api-gw-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as path from "path";
 
 export interface ApiGatewayStackProps extends cdk.StackProps {
   getPresignedUrlFunction: lambda.IFunction;
@@ -23,7 +22,7 @@ export class ApiGatewayStack extends cdk.Stack {
     // Upload URL endpoint
     const presignedUrl = api.root.addResource("upload-url");
     presignedUrl.addMethod(
-      "GET",
+      "POST",
       new apigateway.LambdaIntegration(props.getPresignedUrlFunction, {
         proxy: true,
       })
